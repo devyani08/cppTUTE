8ccb558310539a60785c866941665c93730cda4d
Devyani Arya <devyaniarya5@gmail.com>
2022-08-25 02:31:25 +0530

    CMATH in c++

---
+++ cmath.cpp
@@ -0,0 +1,29 @@
+//square root of a number trial
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	int a;
+	
+	cout<<"enter a number to find square root ";
+	cin>>a;
+	cout<<"square root is:"<<sqrt(a)<<endl;
+	return 0;
+}
+
+/* abs(value) absolute value
+ceil(value) rounds up
+floor(value) rounds down
+log10(value) logarithm, base 10
+max(value1, value2) larger of two values
+min(value1, value2) smaller of two values
+pow(base, exp) base to the exp power
+round(value) nearest whole number
+sqrt(value) square root
+sin(value)
+cos(value)
+tan(value)
+sine/cosine/tangent of an 
+angle in radians*/
